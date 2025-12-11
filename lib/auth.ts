3c6c855437365d2618df884db47093ee8a7ea8af
craniaforceps <chicopereirareis@gmail.com>
2025-12-11 03:46:33 +0000

    fix: Correcção do OAuth do Google

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,11 +2,16 @@ import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from '@/lib/db'
 
+// Variáveis do Google
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 if (!GOOGLE_CLIENT_ID) throw new Error('GOOGLE_CLIENT_ID is not set')
 
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 if (!GOOGLE_CLIENT_SECRET) throw new Error('GOOGLE_CLIENT_SECRET is not set')
+
+// URL da app (env APP_URL)
+const APP_URL = process.env.APP_URL
+if (!APP_URL) throw new Error('APP_URL is not set')
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, { provider: 'postgresql' }),
@@ -15,6 +20,7 @@ export const auth = betterAuth({
     google: {
       clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
+      redirectUri: `${APP_URL}/api/auth/callback/google`, // agora TypeScript reconhece APP_URL
     },
   },
 })
