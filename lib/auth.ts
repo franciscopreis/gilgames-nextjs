ec546c8016b0a47942708372b1595ac1ba2df975
craniaforceps <chicopereirareis@gmail.com>
2025-12-11 03:24:34 +0000

    feat: OAuth do Google integrada

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,12 +2,19 @@ import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
 import prisma from '@/lib/db'
 
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+if (!GOOGLE_CLIENT_ID) throw new Error('GOOGLE_CLIENT_ID is not set')
+
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+if (!GOOGLE_CLIENT_SECRET) throw new Error('GOOGLE_CLIENT_SECRET is not set')
+
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: 'postgresql',
-  }),
-  emailAndPassword: {
-    enabled: true,
+  database: prismaAdapter(prisma, { provider: 'postgresql' }),
+  emailAndPassword: { enabled: true },
+  socialProviders: {
+    google: {
+      clientId: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+    },
   },
-  //   trustedOrigins: ['http://localhost:3001'],
 })
